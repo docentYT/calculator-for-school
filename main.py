ef0c4a3b1d414bddc09684cef30d1a86bf67b2d6
Christopher Kwiatkowski <63965954+docentYT@users.noreply.github.com>
2022-10-15 20:29:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import inspect
+from kalkulator import *
+
+DASH_LINE = "------------------------------------------------------------"
+
+def get_input(anonymous):
+    args = {}
+    for i in inspect.signature(anonymous).parameters: args.update({i: (float(input(f"{i} = "))) })
+    return args
+
+while True:
+    print(DASH_LINE)
+    for key in kalkulator: print('|', list(kalkulator).index(key)+1, "-", key)
+    try:
+        inp = int(input("| Wpisz numer opcji lub \'0\' aby wyjsc: "))
+        if inp == 0: break
+        inpp = get_input(kalkulator[list(kalkulator)[inp-1]])
+        print("Wynik:", kalkulator[list(kalkulator)[inp-1]](**inpp))
+    except: print(DASH_LINE, '\n', "ERROR: Niepoprawne parametry badz numer opcji!")
+
+print(kalkulator["pole powierzchni prostokata"](2, 3))
+    
